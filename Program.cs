f8f0e67f1a97bb3d701ae41b05ae1eba8ee15844
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-01-14 16:19:14 +0900

    Init

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArraySample();
+        }
+
+        private static void ArraySample()
+        {
+            var x = new[] { 9, 15, 32 };
+            x[1] = 4;
+
+            Console.WriteLine(x[0]);
+            Console.WriteLine(x[1]);
+            Console.WriteLine(x[2]);
+
+        }
+    }
+}
