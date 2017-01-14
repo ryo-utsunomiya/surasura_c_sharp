dee2b35c1eb79c48a5b6fa9a5b193c7c62f73de0
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-01-14 16:36:00 +0900

    Add ParamsSample

--- Program.cs
+++ Program.cs
@@ -6,18 +6,44 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            ArraySample();
+            ParamsSample.Run();
         }
 
-        private static void ArraySample()
+        class ArraySample
         {
-            var x = new[] { 9, 15, 32 };
-            x[1] = 4;
+            public static void Run()
+            {
+                var x = new[] { 9, 15, 32 };
+                x[1] = 4;
 
-            Console.WriteLine(x[0]);
-            Console.WriteLine(x[1]);
-            Console.WriteLine(x[2]);
+                Console.WriteLine(x[0]);
+                Console.WriteLine(x[1]);
+                Console.WriteLine(x[2]);
+            }
+        }
 
+        class ParamsSample
+        {
+            public static void Run()
+            {
+                int max = Max(314, 159, 265, 358, 979);
+                Console.WriteLine("{0}", max);
+            }
+
+            private static int Max(params int[] a)
+            {
+                int max = a[0];
+
+                for (int i = 0, len = a.Length; i < len; i++)
+                {
+                    if (a[i] > max)
+                    {
+                        max = a[i];
+                    }
+                }
+
+                return max;
+            }
         }
     }
 }
